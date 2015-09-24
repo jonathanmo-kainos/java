7a90fdd218e740d0f74aaf79fbc726b867dad42b
Jonny Moore <j.moore@kainos.com>
2015-09-24 11:29:11 +0100

    IntelliK Idea files and tax calculation

--- main.java
+++ main.java
@@ -1,7 +1,22 @@
 public class main
 {
-	public static void main(String[] args)
-	{
-		System.out.println("Hello World");
-	}
+    public static void main(String[] args)
+    {
+        String name = "Bob";
+        double salary = 100050;
+        double taxP = 20;
+        System.out.println("Salary with tax: " + Tax(name, salary, taxP));
+
+    }
+
+    public static double Tax(String name, double salary, double taxP)
+    {
+        System.out.println("Starting to calculate tax for " + name);
+        System.out.println("Salary without tax: " + salary);
+        System.out.println("Taking " + taxP + "% off due to tax");
+        salary = salary / 100;
+        salary = salary * (100 - taxP);
+        System.out.println("finished calculating tax for " + name);
+        return salary;
+    }
 }
